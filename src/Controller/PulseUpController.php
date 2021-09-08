f8e7df48400dd0fe58e80d9348ae4309a527beb5
Mehdi Still <mehdi@digiteka.com>
2021-09-08 16:39:18 +0200

    form render

--- src/Controller/PulseUpController.php
+++ src/Controller/PulseUpController.php
@@ -6,23 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\PulseUpTypeFormType;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class PulseUpController extends AbstractController
 {
     /**
      * @Route("/pulseup/form", name="pulse_up")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $form = $this->createForm(PulseUpTypeFormType::class);
 
+        $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $pulseup = $form->getData();
+            $file = $form->get('submitFile')->getData();
 
-            echo "ok";
+            if (($handle = fopen($file->getPathname(), "r")) !== false) {
+                while (($data = fgetcsv($handle)) !== false) {
+                    echo "#".$data[0]."#";
+                }
+                fclose($handle);
+            }
 
-            return $this->redirectToRoute('process');
+           // return $this->redirectToRoute('process',);
         }
 
         return $this->render('pulseup/index.html.twig', [
